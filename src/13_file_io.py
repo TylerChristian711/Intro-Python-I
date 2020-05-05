4746cb064a6b3a7afaf9348013f030cc50376c86
Tyler Christian <tylerc71197@gmail.com>
2020-05-05 17:23:13 -0400

    MVP all goals hit

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -10,6 +10,9 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Note: pay close attention to your current directory when trying to open "foo.txt"
 
 # YOUR CODE HERE
+file = open('foo.txt', 'r')
+print(file.read())
+file.close()
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
@@ -17,3 +20,11 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+bar = open('bar.txt', 'w')
+bar.write("Hello")
+bar.write("from")
+bar.write("Python")
+bar.close()
+openBar = open('bar.txt', 'r')
+print(openBar.read())
+openBar.close()
